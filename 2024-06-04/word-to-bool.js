79b957f21888bd1916b1d6d24471b5f5f5dc2cc0
Marcelo <mfuentes1205@gmail.com>
2024-06-04 10:58:55 -0700

    arrow function review with boolean

---
+++ 2024-06-04/word-to-bool.js
@@ -0,0 +1,15 @@
+// PREP
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    //code here
+}
+hint: can be arrow
+
+
+// P: the parameter is a simple bool
+// R: we wan to return 'Yes' if str is true, else 'No'
+
+// my solution
+
+const boolToWord = bool => bool ? 'Yes' : 'No';
