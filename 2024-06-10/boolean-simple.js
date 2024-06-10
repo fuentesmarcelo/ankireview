9af45e4fe0a8d9d21dd305cc03388a8ad56d24db
Marcelo <mfuentes1205@gmail.com>
2024-06-10 09:48:04 -0700

    reviewing simple boolean functions

---
+++ 2024-06-10/boolean-simple.js
@@ -0,0 +1,15 @@
+// PREP
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    //code here
+}
+hint: can be arrow
+
+// P: we are given a parameter of bool
+// R: we want to return a boolean of true or false
+
+
+
+const boolToWord = bool = bool === true ? 'yes' : 'no';
