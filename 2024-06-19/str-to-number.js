df62d258916aaa08e875ebc214b610e5bfe08038
Marcelo <mfuentes1205@gmail.com>
2024-06-19 12:59:12 -0700

    reviewing how to turn a str to a number

---
+++ 2024-06-19/str-to-number.js
@@ -0,0 +1,19 @@
+// PREP
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+const stringToNumber = function(str){
+    // put your code here
+    return null;
+}
+hint: Number() or parseInt()
+
+// P: the parameter is str
+// R: return a str to a number
+
+
+// my solution
+
+const stringToNumber = function(str){
+    let num = Number(str);
+    return num;
+}
