a551e2039a909a52f403f40f80ffa77532d64187
Marcelo <mfuentes1205@gmail.com>
2024-06-20 12:49:48 -0700

    simple boolean review

---
+++ 2024-06-20/check-if-even-not.js
@@ -0,0 +1,18 @@
+// PREP
+//In this Kata we are passing a number (n) into a function.
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+
+function testEven(n) {
+    //Your awesome code here!
+}
+
+// P: the parameter is n (number)
+// R: return a boolean 
+// E: if n is even return true
+// P: use an if statement
+
+
+// my solution
+
+const testEven = n => n % 2 === 0;
