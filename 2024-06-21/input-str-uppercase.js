c3982205862cde72b52f8c3be4a622bbaba29e59
Marcelo <mfuentes1205@gmail.com>
2024-06-21 12:43:28 -0700

    return a str with an uppercase

---
+++ 2024-06-21/input-str-uppercase.js
@@ -0,0 +1,15 @@
+// PREP
+//Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+}
+hint: do it regular way or arrow function
+
+// P: your parameter is a str
+// R: return the str in all uppercase
+// E: 'words' => 'WORDS'
+// P: solve this one using an arrow function 
+
+
+const makeUpperCase = str => str.toUpperCase();
