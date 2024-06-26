cd21abd53e9d24d0d971501f23b6391b155b6f6a
Marcelo <mfuentes1205@gmail.com>
2024-06-26 11:35:59 -0700

    review on returning the reverse of a string and printing it to the console

---
+++ 2024-06-26/reverse-str.js
@@ -0,0 +1,21 @@
+// PREP
+//Create a function that takes string
+//Print the reverse of that string to the console
+
+hint: .split and .reverse and .join with arrow function
+
+// P: the parameter is going to be a string
+// R: we are trying to return the reverse of that string to the console
+// E: 'bat' => 'tab'
+// P: we are going to solve this using an arrow function
+// we need a function name
+// we need a str
+// we need to use .split and .reverse and .join 
+
+
+
+
+
+// my solution
+
+const reverseStr = str = console.log(str.split('').reverse().join(''));
