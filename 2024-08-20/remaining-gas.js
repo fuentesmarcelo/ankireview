c2e370b4ab6dc5ea655a9346202b26e3737cdcc3
Marcelo <mfuentes1205@gmail.com>
2024-08-20 15:32:03 -0700

    used arrow function to solve mpg to fuel problem

---
+++ 2024-08-20/remaining-gas.js
@@ -0,0 +1,15 @@
+// PREP
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    //code here
+};
+//hint: can use arrow
+
+// P: we have three parameters 
+// R: we want to find out if our car will make it to the pump
+
+
+// my solution
+
+const zeroFuel = mpg * fuelLeft >= distanceToPump;
