9b1cecb6dfa22520cfcaae661f6d730decdcfdb6
Marcelo <mfuentes1205@gmail.com>
2024-08-21 11:53:03 -0700

    checking if boolean is a str

---
+++ 2024-08-21/boolean-value-str.js
@@ -0,0 +1,15 @@
+// PREP
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    //code here
+    }
+hint: can be arrow
+
+// P: the str is a boolean
+// R: we want to return a boolean value as true str
+
+
+// my solution
+
+const boolToWord = bool => bool ? 'yes' : 'no';
