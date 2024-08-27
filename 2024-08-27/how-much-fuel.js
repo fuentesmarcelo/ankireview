6fb707953cfccc8f5336255c68dd29f7d803e8b9
Marcelo <mfuentes1205@gmail.com>
2024-08-27 15:10:05 -0700

    figured out how much fuel is left to pump station

---
+++ 2024-08-27/how-much-fuel.js
@@ -0,0 +1,16 @@
+// PREP
+
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    //code here
+};
+hint: can use arrow
+
+
+
+
+
+// my solution
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
