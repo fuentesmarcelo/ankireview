704a7527be9ff0b2b36f0ee41c4f1952223642d2
Marcelo <mfuentes1205@gmail.com>
2024-08-30 10:23:54 -0700

    using back ticks to finish a sentece depending on outcome

---
+++ 2024-08-30/chrom-check-sperm.js
@@ -0,0 +1,25 @@
+// PREP
+// The sperm cell determines the sex of an individual in this case. If a sperm cell containing an X chromosome fertilizes an egg, the resulting zygote will be XX or female. If the sperm cell contains a Y chromosome, then the resulting zygote will be XY or male.
+// If the sperm contains the X chromosome, return "Congratulations! You're going to have a daughter."; If the sperm contains the Y chromosome, return "Congratulations! You're going to have a son.";
+
+function chromosomeCheck(sperm) {
+}
+
+hint: back ticks
+
+
+
+// my solution
+
+function chromosomeCheck(sperm){
+    return (`Congratulations, You're going to have a ${son}.` ? XY : XX);
+}
+
+
+
+// right solution
+
+function chromosomeCheck(sperm) {
+    return `Congratulations! You're going to have a ${sperm === 'XY' ? 'son' : 'daughter'}.`
+}
+  
