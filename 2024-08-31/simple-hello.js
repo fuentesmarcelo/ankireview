6b2ab932abf0eba36266daed5048b0f333b32daa
Marcelo <mfuentes1205@gmail.com>
2024-08-31 10:54:42 -0700

    simple response given an owner and name parameter

---
+++ 2024-08-31/simple-hello.js
@@ -0,0 +1,14 @@
+// PREP
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+// Use conditionals to return the proper message:'hello boss' or 'hello guest
+
+function greet (name, owner) {
+    //code here
+}
+
+
+// my solution
+
+const greeting = (name, owner) => name === owner ? 'hello boss' : 'hello guest';
+
+   
