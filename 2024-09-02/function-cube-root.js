8cfb5406e966c62cc133c63eee55c81763f1a5e8
Marcelo <mfuentes1205@gmail.com>
2024-09-02 10:55:24 -0700

    return the cube root of a number

---
+++ 2024-09-02/function-cube-root.js
@@ -0,0 +1,23 @@
+// PREP
+
+// Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+
+hint: Math.cbrt()
+
+
+// P: the parameter is going to be a numbeer
+// R: we want to return the number's cube root
+
+
+// my solution
+
+function numToCube(num){
+    let cubeNum = num.Math.cbrt(num);
+    console.log(cubeNum)
+}
+
+// right solution
+
+function cubeRootIt(n1){
+    console.log(Math.cbrt(n1))
+}
