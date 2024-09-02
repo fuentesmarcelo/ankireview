79c0251d1dc1fb01f9af273bb8ba3af9154a1aee
Marcelo <mfuentes1205@gmail.com>
2024-09-02 11:09:28 -0700

    reversing a string

---
+++ 2024-09-02/solution-str.js
@@ -0,0 +1,15 @@
+// PREP
+//Complete the solution so that it reverses the string passed into it.
+function solution(str){
+    //code here
+}
+// hint: split, reverse, join can be solved an an arrow
+
+// P: the parameter is a str
+// R: reverse the str
+
+// my solution
+
+function solution(str){
+    return str.join('').reverse().join('');
+}
