e4db8feb667350c7a05a8f293d10423828e95401
Marcelo <mfuentes1205@gmail.com>
2024-09-04 13:13:10 -0700

    smashing words together using .join

---
+++ 2024-09-04/smash-words.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+function smash (words) {
+    return ""
+}
+//hint: .join() can be solved via arrow
+
+// P: the parameter is words 
+// R: we want to return the words smashed together with spaces between each word
+// E: n/a
+// P: solve using an arrow function
+// we will use .join on the parameter of words
+
+
+// my solution
+
+const smash = words => words.join('');
