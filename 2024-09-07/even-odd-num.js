f14006ea2914a9d2e1b66d70f50078951287278b
Marcelo <mfuentes1205@gmail.com>
2024-09-07 12:30:34 -0700

    simple boolean that's either even or odd

---
+++ 2024-09-07/even-odd-num.js
@@ -0,0 +1,13 @@
+// PREP
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+hint: if or arrow modular
+
+// P: the parameter is going to be an integer
+// R: we want to return 'even' for even nums or 'odd' for odd nums
+// E: n/a
+// P: make an arrow function with a boolean
+
+// my solution
+
+const evenOrOdd = num => num % 2 === 0 ? 'even' : 'odd';
