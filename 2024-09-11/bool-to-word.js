fdaf2d2cd9bf6c7e3fd5df43a37fd67885acb31d
Marcelo <mfuentes1205@gmail.com>
2024-09-11 13:32:19 -0700

    checking boolean if true or false

---
+++ 2024-09-11/bool-to-word.js
@@ -0,0 +1,17 @@
+// PREP
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    //code here
+}
+// hint: can be arrow
+
+
+// P: the parameter is a bool 
+// R: we want to return 'yes' if string is true or 'no' if false
+// E: n/a
+// P: solve using the arrow function with a conditional
+
+// my solution
+
+const boolToWord = bool => bool ? 'yes' : 'no';
