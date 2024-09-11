8d6b943386bedd9636e2291dc02fb78cb7ccb7bb
Marcelo <mfuentes1205@gmail.com>
2024-09-11 14:19:51 -0700

    using a forEach loop on an arr

---
+++ 2024-09-11/tv-shows-forEach.js
@@ -0,0 +1,16 @@
+// PREP
+
+//Create and array of tv shows. Use forEach loop through and print each show to the console
+
+// P: the parameter is an array of tv shows
+// R: we want to run a forEach loop and print each show to the console
+// E: n/a
+// P: make a function with an arr
+// run the forEach loop within
+
+
+// my solution
+
+function tvShow(arr){
+    forEach((show) => console.log(arr))
+}
