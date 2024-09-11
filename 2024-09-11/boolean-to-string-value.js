dc319cedb654e3f8bd4c0e9fce84da058929b0b3
Marcelo <mfuentes1205@gmail.com>
2024-09-11 14:11:46 -0700

    checking for a boolean making a string

---
+++ 2024-09-11/boolean-to-string-value.js
@@ -0,0 +1,31 @@
+// PREP
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //code here
+}
+//hint: if statement
+
+// P: the parameter is going to be boolean value
+// R: we want to return the boolean value as a string
+// E: n/a
+// P: we will use an if statement to solve this
+
+
+// my solution
+
+function booleanToString(b){
+    if(b === string){
+        return 'string'
+    }
+}
+
+// right solution
+
+function booleanToString(b){
+    if(b === true){
+      return 'true'
+    }else{
+      return 'false'
+    }
+  }
