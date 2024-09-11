dd34319440fd196f2fb984393df186ea0b80265d
Marcelo <mfuentes1205@gmail.com>
2024-09-11 14:16:07 -0700

    checking for a word using .search

---
+++ 2024-09-11/search-str-words.js
@@ -0,0 +1,28 @@
+// PREP
+
+//Declare a variable, assign it a string of multiple words, and check to see if one of the words is "apple".
+
+hint: .search()
+
+
+// P: 
+// R:
+// E:
+// P:
+
+// my solution
+
+let str = words;
+
+if(str.search('apple'))
+
+
+// right solution
+
+
+let str = 'Bob Duck apple dog'
+if(str.search('apple') !== -1){
+console.log('yes')
+}else{
+console.log('no')
+}
