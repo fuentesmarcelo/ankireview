a41500ef302a1b2e85550d8d406ffed52dda1fd3
Marcelo <mfuentes1205@gmail.com>
2024-09-12 16:20:33 -0700

    reversing my parameter and printing it to a log

---
+++ 2024-09-12/misty-party-order.js
@@ -0,0 +1,16 @@
+// PREP
+
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+hint: .reverse() with arrow or without
+
+// P: the parameter will be partyOrder
+// R: we want to return my parameter in reverse and print it to the console
+// E: n/a
+//  going to solve using an arrow function
+// going to attach my team to .reverse
+
+
+// my solution
+
+const mitsyParty = partyOrder => console.log(partyOrder.reverse());
