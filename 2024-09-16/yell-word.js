076118b1d36fe4f58349f91428884f47136a708c
Marcelo <mfuentes1205@gmail.com>
2024-09-16 15:43:55 -0700

    alerting a word

---
+++ 2024-09-16/yell-word.js
@@ -0,0 +1,17 @@
+// PREP
+
+// How do you call this function?
+
+function yell(word){
+  alert(word)
+}
+
+// P: the parameter is a word
+// R: all you have to do is return the word
+// E: n/a
+// P: n/a
+
+
+// my solution
+
+yell('hello')
