df838d44642f7adfab24154f1b1c62bd9f6193cb
Marcelo <mfuentes1205@gmail.com>
2024-09-16 15:38:41 -0700

    reversing a string with an arrow function

---
+++ 2024-09-16/solutions-string.js
@@ -0,0 +1,19 @@
+// PREP
+
+//Complete the solution so that it reverses the string passed into it.
+function solution(str){
+    //code here
+}
+// hint: split, reverse, join can be solved an an arrow
+
+
+
+// P: the parameter is a string 
+// R: we want to reverse the string 
+// E: n/a
+// P: solving via arrow function with split, reverse, join
+
+
+// my solution
+
+const solution = str => str.split('').reverse().join('');
