93580504664eb5f66e66896dfbbefc3612ef543f
Marcelo <mfuentes1205@gmail.com>
2024-09-16 15:34:36 -0700

    alerting the last three characters of a string using slice

---
+++ 2024-09-16/last-three.js
@@ -0,0 +1,19 @@
+// PREP
+
+//Declare a variable, assign it a string, alert the last three characters in the string
+
+hint: slice()
+
+
+// my solution
+
+let str = 'hello';
+
+alert(str);
+
+
+// right solution
+
+
+let favPowerRangersSeries = 'Alien Rangers'
+alert(favPowerRangersSeries.slice(-3))
