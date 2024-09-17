15550392f137e326a41a2110b793e0b8603934e8
Marcelo <mfuentes1205@gmail.com>
2024-09-17 14:50:31 -0700

    solving with an arrow function that repeats

---
+++ 2024-09-17/repeat-str.js
@@ -0,0 +1,19 @@
+// PREP
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    //code here
+}
+//hint: .repeat() can be solved as an arrow function
+
+
+// P: the parameter is a number and a string
+// R: we want to return the string repeated exactly n times
+// E: n/a
+// P: solving using an arrow function with .repeat()
+
+
+// my solution
+
+const repeatStr = (n, s) => s.repeat(n);
