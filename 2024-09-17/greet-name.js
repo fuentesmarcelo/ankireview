1978137cf61bc500de426d3e1fef1a98f306569f
Marcelo <mfuentes1205@gmail.com>
2024-09-17 14:45:53 -0700

    returning a message with back ticks

---
+++ 2024-09-17/greet-name.js
@@ -0,0 +1,20 @@
+// PREP
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+    //code here
+}
+//hint: back ticks can be solved as an arrow function
+
+// P: the parameter is a name
+// R: we want to return a message 
+// E: n/a
+// P: n/a
+
+
+// my solution
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
