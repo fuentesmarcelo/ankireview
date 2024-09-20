64e3249a2f310413aefcc65b3a1a135385a7c234
Marcelo <mfuentes1205@gmail.com>
2024-09-20 14:46:39 -0700

    we are returning the template literals using back ticks

---
+++ 2024-09-20/temp-str-literal.js
@@ -0,0 +1,19 @@
+// PREP
+
+// Your task is to return the correct string using the Template String Feature.
+
+var templateStrings = function(noun, adjective) {
+    return //complete this
+}
+
+// P: the parameter is a noun and adjective
+// R: we want to return the string with template string feature
+// E: n/a
+// P: return with the back ticks and template literals
+
+
+// my solution
+
+var templateStrings = function(noun, adjective) {
+    return `${noun} + ${adjective}`
+}
