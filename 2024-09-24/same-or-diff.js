cfa9494dedf08556acce8b4a83479fae4911cf01
Marcelo <mfuentes1205@gmail.com>
2024-09-24 17:33:03 -0700

    checking two variables if they are the same num or string

---
+++ 2024-09-24/same-or-diff.js
@@ -0,0 +1,16 @@
+// PREP
+
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+// To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
+
+function add(a, b){
+    // code here
+}
+
+
+// P: the parameter is (a, b) with one num and the other string
+// R: return true if they are the same character(regardless of type)
+
+
+const add = (a, b) => a==b;
