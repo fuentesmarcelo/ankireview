9376846f7d0f81ca3bcceab5d5ebb24e8699662b
Marcelo <mfuentes1205@gmail.com>
2024-09-25 17:36:52 -0700

    checking if a num if even or odd and solved with an arrow function

---
+++ 2024-09-25/test-even.js
@@ -0,0 +1,20 @@
+// PREP
+
+//In this Kata we are passing a number (n) into a function.
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+
+function testEven(n) {
+    //Your awesome code here!
+}
+
+
+// P: the parameter is a number
+// R: we wamt to check if the number is even or odd
+
+
+
+
+// my solution
+
+const testEvene = n => n % 2 === 0 ? true : false;
