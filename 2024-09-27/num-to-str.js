76355cd9c554d58b36493f8ea0aa4c02329aebad
Marcelo <mfuentes1205@gmail.com>
2024-09-27 15:18:34 -0700

    turning a number into a string

---
+++ 2024-09-27/num-to-str.js
@@ -0,0 +1,18 @@
+// PREP
+
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+}
+// hint: .toString();
+
+// P: the paramter is a number
+// R: we want to transform the number into a string
+// E: 42 => '42'
+// P: just do a return with toString
+
+
+// my solution
+
+const numberToString = num => num.toString();
