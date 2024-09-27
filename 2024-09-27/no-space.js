ebbad482f8da0102695be0f1a4ec2d77014c8955
Marcelo <mfuentes1205@gmail.com>
2024-09-27 16:04:41 -0700

    trying to return the string with no spaces

---
+++ 2024-09-27/no-space.js
@@ -0,0 +1,21 @@
+// PREP
+
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    //code here
+}
+//hint: .replace() and can be solved an an arrow function
+
+
+// P: the parameter is a string
+// R: we want to return the string with the spaces removed
+// E: n/a
+// P: n/a
+
+
+// my solution
+
+function noSpace(x){
+    return x.replace(/ /gi, ' ');
+}
