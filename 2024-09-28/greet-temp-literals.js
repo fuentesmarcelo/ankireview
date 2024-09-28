bba44ae7b00e01749e752ffd0fe94354564f124b
Marcelo <mfuentes1205@gmail.com>
2024-09-28 11:33:02 -0700

    using template literals with back ticks to return a message

---
+++ 2024-09-28/greet-temp-literals.js
@@ -0,0 +1,18 @@
+// PREP
+
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+function greet(name){
+    //code here
+}
+//hint: back ticks can be solved as an arrow function
+
+
+
+
+// my solution
+
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+}
