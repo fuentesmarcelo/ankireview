8e6f06c17d87a1db5d64a8faf4198e03e386e99b
Marcelo <mfuentes1205@gmail.com>
2024-10-01 14:00:10 -0700

    solved with an arrow function how returning an array with only the positive numbers

---
+++ 2024-10-01/evens-only.js
@@ -0,0 +1,14 @@
+// PREP
+
+//Create and array of numbers
+//Return a new array of numbers that includes every even number from the previous Arrays
+
+function onlyEvens(arr){
+    //code here
+}
+//hint: .filter() and arrow function
+
+
+// my solution
+
+const onlyEvens = arr => arr.filter(n % 2 === 0);
