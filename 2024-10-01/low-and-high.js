6516d5d9b56c4e6ed2526dcc8fd9942c4e8a5880
Marcelo <mfuentes1205@gmail.com>
2024-10-01 11:55:01 -0700

    returning the min and max of the parameters given

---
+++ 2024-10-01/low-and-high.js
@@ -0,0 +1,14 @@
+//Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function threeNumsLowAndHigh (n1,n2,n3) {
+    //code here
+}
+//hint: min and max
+
+
+// my solution
+
+function threeNumsLowAndHigh (n1,n2,n3) {
+    let min = Math.min(n1, n2, n3);
+    let max = Math.max(n1, n2, n3)
+    return `${min} + ${max}`
+}
