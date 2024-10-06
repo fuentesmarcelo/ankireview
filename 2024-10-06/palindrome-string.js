e1434ffce4dc6aa239c671256fbf6c1cdc1f8291
Marcelo <mfuentes1205@gmail.com>
2024-10-06 13:34:32 -0700

    checking if a string is a palindrome

---
+++ 2024-10-06/palindrome-string.js
@@ -0,0 +1,11 @@
+//Create a function that takes in a string
+//Alert if the string is a palindrome or not
+
+// hint: split, reverse, join, arrow function
+
+
+// the parameter is going to be a string
+// we want to alert the string if its a palindrome or not
+
+
+const isPal = str => str.split('').reverse().join('');
