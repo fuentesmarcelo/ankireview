aa708e5111304b445cd3bf053419607b807ec5dc
Marcelo <mfuentes1205@gmail.com>
2024-10-07 14:04:03 -0700

    multiplying the parameter by a specific number if even or odd

---
+++ 2024-10-07/simple-multi-num.js
@@ -0,0 +1,12 @@
+//This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+function simpleMultiplication(number) {
+    // your code........
+}
+
+// hint: can use arrow function
+
+
+// my solution
+
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9;
