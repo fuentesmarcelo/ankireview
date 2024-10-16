140c97d5676fa26fb0a31100e827f8acb6ccb0df
Marcelo <mfuentes1205@gmail.com>
2024-10-16 13:49:08 -0700

    finding the average of an array

---
+++ 2024-10-16/find-arr-average.js
@@ -0,0 +1,17 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+function findAverage(array) {
+    // your code here
+    return 0;
+}
+// hint: if statement, declare variable, for loop or arrow with reduce
+
+
+
+// my solution
+
+const findAverage = array => array.reduce((acc, c) => acc + c, 0)/array.length;
+
+// right solution
+
+const find_average = array => array.reduce((acc, curr) => acc + curr, 0) / array.length || 0;
