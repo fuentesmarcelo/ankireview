54df478b07cc1001dd698e778b51057a0c458678
Marcelo <mfuentes1205@gmail.com>
2024-10-22 13:12:25 -0700

    two parameters, str and num, repeating str by num

---
+++ 2024-10-22/repeat-string.js
@@ -0,0 +1,11 @@
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr (n, s) {
+    //code here
+}
+//hint: .repeat() can be solved as an arrow function
+
+
+// my solution
+
+const repeatStr = (n, s) => s.repeat(n);
