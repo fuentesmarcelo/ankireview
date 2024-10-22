162f9e09d29ce9fea8eb7a9d9935e3dc27ea1613
Marcelo <mfuentes1205@gmail.com>
2024-10-22 13:29:29 -0700

    returning the str repre of a boolean

---
+++ 2024-10-22/boo-to-str.js
@@ -0,0 +1,27 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //code here
+}
+//hint: if statement
+
+
+
+// my solution
+
+function booleanToString(b){
+    if(b == str){
+        return str
+    }
+}
+
+
+// right solution
+
+function booleanToString(b){
+    if(b === true){
+      return 'true'
+    }else{
+      return 'false'
+    }
+  }
