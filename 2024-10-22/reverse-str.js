91cf4f7a618e83853f64f060f2926ab0f6e1fb0a
Marcelo <mfuentes1205@gmail.com>
2024-10-22 13:40:30 -0700

    reversing a string via arrow function

---
+++ 2024-10-22/reverse-str.js
@@ -0,0 +1,12 @@
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+function reverse(string){
+    //your code here
+}
+//hint can be solved via arrow function
+
+
+// my solution
+
+const reverse = string => string.split(' ').reverse().join('');
