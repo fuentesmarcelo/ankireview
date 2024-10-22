f59e2de275a05eb89b338bf3f87a114836b61625
Marcelo <mfuentes1205@gmail.com>
2024-10-22 14:07:44 -0700

    checking two conditions to return status of members

---
+++ 2024-10-22/senior-old.js
@@ -0,0 +1,22 @@
+//To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+//Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+//Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+function openOrSenior(data){
+    // ...
+}
+//hint: .map and arrow boolean
+
+
+
+// my solution
+
+const openOrSenior = data => age > 55 && handicap > 7 ? Senior : open;
+
+// right solution
+
+function openOrSenior(data){
+    return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+   }
+
+   const openOrSenior = data => data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
