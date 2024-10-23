c44a968249e28e813b4d2e90e78b54e9dbbea794
Marcelo <mfuentes1205@gmail.com>
2024-10-23 12:25:37 -0700

    filtering out a specific variable from function

---
+++ 2024-10-23/goose.js
@@ -0,0 +1,24 @@
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    // return an array containing all of the strings in the input array except those that match strings in geese
+};
+// hint: filter and indexOf or filter and .includes
+
+
+
+// my solution
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter(birds => birds.indexOf(geese) !== -1)
+    // return an array containing all of the strings in the input array except those that match strings in geese
+};
+
+// right solution
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    return birds.filter( bird => geese.indexOf(bird) < 0 );
+    };
