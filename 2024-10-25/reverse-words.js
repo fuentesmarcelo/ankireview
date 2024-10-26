e4792da550ded96561a95274f9569493ebd40278
Marcelo <mfuentes1205@gmail.com>
2024-10-25 17:21:18 -0700

    reversing a str of words using an arrow function

---
+++ 2024-10-25/reverse-words.js
@@ -0,0 +1,11 @@
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+function reverseWords(str){
+    return str; // reverse those words
+}
+
+
+// my solution
+
+const reverseWords = str => str.split('').reverse().join('');
