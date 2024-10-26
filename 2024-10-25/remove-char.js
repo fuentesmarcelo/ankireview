02b2af9ae08c7507b564589c0bd827f77de646a0
Marcelo <mfuentes1205@gmail.com>
2024-10-25 17:19:49 -0700

    solving via arrow function removing first and last character of a string

---
+++ 2024-10-25/remove-char.js
@@ -0,0 +1,12 @@
+//Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
+function removeChar(str){
+    //You got this!
+    
+}
+// hint: slice() also with arrow function
+
+
+// my solution
+
+const removeChar = str => str.slice(1, -1);
