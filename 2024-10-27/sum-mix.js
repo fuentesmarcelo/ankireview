02fd5374a4b25c56899f39df1d467635eabc83a8
Marcelo <mfuentes1205@gmail.com>
2024-10-27 12:24:02 -0700

    returning an array with the sum of the values all

---
+++ 2024-10-27/sum-mix.js
@@ -0,0 +1,18 @@
+//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+//Return your answer as a number.
+
+function sumMix(x){
+    //code here
+}
+//hint: reduce and can be solved as an arrow (two plus)
+
+
+
+// my solution
+
+const sumMix = x => x.reduce((a, b) => +a + b, 0);
+
+
+// right solution
+
+const sumMix=x=>x.reduce((a,b)=>+b+a,0);
