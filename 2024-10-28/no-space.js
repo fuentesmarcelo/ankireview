b9d159f8ede2b133c714fbda32f0cbbb8aba670f
Marcelo <mfuentes1205@gmail.com>
2024-10-28 13:28:46 -0700

    removing white spaces from string with arrow function

---
+++ 2024-10-28/no-space.js
@@ -0,0 +1,16 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+    //code here
+}
+//hint: .replace() and can be solved an an arrow function
+
+
+// my solution
+
+const noSpace = x => x.replace(/./gi, '');
+
+
+// right solution
+
+const noSpace = x => x.replace(/ /gi, '');
