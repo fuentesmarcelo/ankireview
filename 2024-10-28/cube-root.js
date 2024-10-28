ccc740112e533b424b79d3feac2489513b16ce71
Marcelo <mfuentes1205@gmail.com>
2024-10-28 14:01:29 -0700

    taking a cube root of the number and calling it

---
+++ 2024-10-28/cube-root.js
@@ -0,0 +1,15 @@
+// Create a function that takes in 1 number. Console log the cube root of the number. Call the function.
+
+function cubeRootIt(n1){
+    //code here
+}
+//hint: Math.cbrt()
+
+
+// my solution
+
+function cubeRootIt(n1){
+    let cubeNum = Math.cbrt(n1);
+    console.log(cubeNum);
+}
+
