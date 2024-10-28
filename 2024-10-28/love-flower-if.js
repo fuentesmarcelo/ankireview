67a3cf684371bf0310f298f59c3908e53e673f1b
Marcelo <mfuentes1205@gmail.com>
2024-10-28 12:32:20 -0700

    if statement to determine a boolean

---
+++ 2024-10-28/love-flower-if.js
@@ -0,0 +1,27 @@
+// If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+    // moment of truth
+}
+
+
+// my solution
+
+function lovefunc(flower1, flower2){
+    if(flower1 === even && flower2 === odd || flower1 === odd && flower2 === even){
+        return 'true';
+    }else{
+        return 'false';
+    }
+}
+
+
+// right solution
+
+function lovefunc(flower1, flower2){ 
+    if ((flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0)) 
+        { return true; 
+    } else { return false; 
+    } 
+}
