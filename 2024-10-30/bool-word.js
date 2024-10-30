c1d102edefcd6bf4601ccf0c67a72b72f523e0aa
Marcelo <mfuentes1205@gmail.com>
2024-10-30 10:22:35 -0700

    checking if a string a boolean

---
+++ 2024-10-30/bool-word.js
@@ -0,0 +1,16 @@
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+    //code here
+}
+// hint: can be arrow
+
+
+
+//  my solution
+
+const boolToWord = bool => str === bool;
+
+// right solution
+
+const boolToWord = bool => bool ? 'Yes' : 'No';
