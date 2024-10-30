77d90b4a77f2157e43e44b2a15d8fcfbb46cfb26
Marcelo <mfuentes1205@gmail.com>
2024-10-30 10:49:59 -0700

    checking if a string word is a palindrome via arrow function

---
+++ is-pali.js
@@ -0,0 +1,12 @@
+// create a function that takes in a word and the function should tell us if that word is a palindrome
+function isPalidrome(word){
+    // code here
+}
+//hint: can be solved as arrow function
+
+
+// my solution
+
+const isPalidrome = word => word === word.split('').reverse().join('') ? 'Yes': 'No';
+
+const isPalidrome = word => word === word.split('').reverse().join('');
