6778436960dab4219a200451b97e40931648c391
Marcelo <mfuentes1205@gmail.com>
2024-11-04 11:59:26 -0800

    returning a string as an array with length attached to it

---
+++ 2024-11-04/add-length.js
@@ -0,0 +1,21 @@
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
+// Note: String will have at least one element; words will always be separated by a space.
+
+function addLength(str) {
+    //start-here
+}
+// hint: .split, .map, .length and make it an arrow
+
+
+
+// my solution
+
+function addLength(str) {
+    return str.split('').map(str => str.length);
+}
+
+// right solution
+
+function addLength(str) {
+    return str.split(' ').map(word => word + ' ' + word.length)
+}
