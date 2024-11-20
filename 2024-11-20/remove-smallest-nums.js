fdf893b88291dd809549a27748fdab1a3ffb5a53
Marcelo <mfuentes1205@gmail.com>
2024-11-20 12:33:54 -0800

    trying to solve this finding the smallest value

---
+++ 2024-11-20/remove-smallest-nums.js
@@ -0,0 +1,28 @@
+//Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list. Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+    return [];
+}
+//hint: reg fun: declare var, indexOf, Math.min, slice
+//hint: arrow: filter, indexOf, Math.min
+
+
+// my solution
+
+function removeSmallest(numbers) {
+    let sortedNum = numbers.sort((a, b) => a - b);
+    return 
+    return [];
+}
+
+
+// right solution
+
+function removeSmallest(numbers) {
+    let indexOfMin = numbers.indexOf(Math.min(...numbers));
+    return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+}
+
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
+
+  
