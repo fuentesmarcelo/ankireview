52cb77386dc9406e9c14f8bcb8c4b3ae73c7ca5e
Marcelo <mfuentes1205@gmail.com>
2024-11-23 16:09:39 -0800

    made a string into upper case via arrow function

---
+++ 2024-11-23/make-upper-case.js
@@ -0,0 +1,11 @@
+//Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+}
+// hint: do it regular way or arrow function
+
+
+// my solution
+
+const makeUpperCase = str => str.toUpperCase();
