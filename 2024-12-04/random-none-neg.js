26170bb9b44d87e247442f0adcbd08a166f79840
Marcelo <mfuentes1205@gmail.com>
2024-12-04 12:41:25 -0800

    returning numbers within an array in reverse order

---
+++ 2024-12-04/random-none-neg.js
@@ -0,0 +1,19 @@
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+function digitize(n) {
+    //code here
+}
+//hint: String, split, map, reverse
+
+
+// my solution
+
+function digitize(n) {
+    return String(n).split('').map(num => num % 2 === 0).reverse();
+}
+
+// right solution
+
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+    }
