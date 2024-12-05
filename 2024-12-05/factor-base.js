8e7870069fd2bfe42bd5ec386e6f1a7d73ea7cf6
Marcelo <mfuentes1205@gmail.com>
2024-12-05 12:47:33 -0800

    checking if a factor of a base is true

---
+++ 2024-12-05/factor-base.js
@@ -0,0 +1,11 @@
+//This function should test if the factor is a factor of base.
+//Return true if it is a factor or false if it is not.
+// hint: use mod operator % could use arrow
+
+// my solution
+
+const factorOfBase = (factor, base) => factor % base === 0 ? true : false;
+
+// right solution
+
+const checkForFactor = (base, factor) => base % factor === 0;
