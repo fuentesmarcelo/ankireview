8e809e72989e740fe09c6ac457b381795c2f6e2b
Marcelo <mfuentes1205@gmail.com>
2024-12-10 14:07:50 -0800

    reversing party order

---
+++ 2024-12-10/misty-party.js
@@ -0,0 +1,6 @@
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+// hint: .reverse() with arrow or without
+
+// my solution
+
+const mistyParty = pokemon => pokemon.reverse()
