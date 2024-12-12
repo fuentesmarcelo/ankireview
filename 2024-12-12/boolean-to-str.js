5024103bfd09a0bda149721e3cea2115f7b54949
Marcelo <mfuentes1205@gmail.com>
2024-12-12 12:40:24 -0800

    using an if statement to check if a boolean is a string

---
+++ 2024-12-12/boolean-to-str.js
@@ -0,0 +1,27 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //code here
+}
+//hint: if statement
+
+// my solution 
+
+function booleanToString(b){
+    if(b == string){
+        return 'true'
+    }else{
+        return 'false'
+    }
+}
+
+
+// right solution
+
+function booleanToString(b){
+    if(b === true){
+      return 'true'
+    }else{
+      return 'false'
+    }
+  }
