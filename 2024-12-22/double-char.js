fd3b32669854b9d7e9976080d286e85aca332653
Marcelo <mfuentes1205@gmail.com>
2024-12-22 10:26:21 -0800

    repeating each character of a string

---
+++ 2024-12-22/double-char.js
@@ -0,0 +1,14 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+    // Your code here
+}
+// hint: can be solved using arrow with split, map, join or solved regular with for loop
+
+//  my solution
+
+const doubleChar = str => str.split('').map(word => word + '' + ).join('')
+
+// right solution
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
