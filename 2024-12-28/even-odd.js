87e2e6fd5dc5277fc295ade9b5825cf74870d602
Marcelo <mfuentes1205@gmail.com>
2024-12-28 14:13:55 -0800

    checking if a num is even or odd

---
+++ 2024-12-28/even-odd.js
@@ -0,0 +1,13 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// hint: if or arrow modular
+
+
+// my solution
+
+const numOrNums = num => num % 2 === 0 ? 'Even' : 'Odd';
+
+
+// right solution
+
+const evenOrOdd = (number) => number % 2 ? 'Even' : 'Odd'; 
