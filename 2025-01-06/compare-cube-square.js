e8922a9578fa56d70776e8a5a0397f5e1a9b09c3
Marcelo <mfuentes1205@gmail.com>
2025-01-06 11:44:11 -0800

    checking the sum of a cube and square of the parameters

---
+++ 2025-01-06/compare-cube-square.js
@@ -0,0 +1,21 @@
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+function compareSquareAndCube(a, b){
+    //code here
+}
+// hint: .reduce() twice
+
+
+//  my solution
+
+function compareSquareAndCube(a, b){
+    return a.reduce((a, b) => a - b) + b.reduce((a, b) => a - b);
+} 
+
+
+// right solution
+
+
+function compareSquareAndCube(a, b){
+    return a.reduce((acc, c) => acc + c **2, 0) > b.reduce((acc, c) => acc + c **3, 0)
+    }
