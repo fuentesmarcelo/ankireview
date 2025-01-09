4789b17b7561216ebc88294dbda3970defa71907
Marcelo <mfuentes1205@gmail.com>
2025-01-09 14:14:07 -0800

    rounding a number and timing it by 5

---
+++ 2025-01-09/round-5.js
@@ -0,0 +1,27 @@
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+function roundToNext5(n){
+    // ...
+}
+// hint: math.ceil or while loop also can be solved as arrow
+
+
+// my solution
+
+function roundToNext5(n){
+    return Math.ceil(1/n)*5
+}
+
+
+// right solution
+
+function roundToNext5(n){
+    return Math.ceil(n/5)*5;
+    }
+    
+    function roundToNext5(n){
+    while(n % 5 !== 0) n++;
+    return n;
+    }
+    
+    const roundToNext5 = n => Math.ceil(n / 5) * 5;
