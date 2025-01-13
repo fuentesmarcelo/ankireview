93c9f24a4642e891bb4e96b45640102866e9f7db
Marcelo <mfuentes1205@gmail.com>
2025-01-13 14:51:41 -0800

    returning a number into a string

---
+++ 2025-01-13/num-string.js
@@ -0,0 +1,13 @@
+// We need a function that can transform a number (integer) into a string.
+
+function numberToString(num) {
+    // Return a string of the number here!
+}
+// hint: .toString();
+
+
+// my solution
+
+function numberToString(num) {
+    return num.toString()
+}
