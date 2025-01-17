5ea428af029ec46987356dd24e3cb6c3f3379e35
Marcelo <mfuentes1205@gmail.com>
2025-01-17 12:08:56 -0800

    checking if a booleean is a stting

---
+++ 2025-01-17/boolean-string.js
@@ -0,0 +1,20 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //code here
+}
+//hint: if statement
+
+// my solution
+
+function booleanToString(b){
+    if(b == true){
+        return string
+    }
+}
+
+// right solution
+
+function booleanToString(b) {
+    return b.toString();
+}
