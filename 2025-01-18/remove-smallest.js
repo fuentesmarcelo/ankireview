3c64f7a3b1a80dc44eacb7986df70a64b69b77a3
Marcelo <mfuentes1205@gmail.com>
2025-01-18 12:16:08 -0800

    using arrow to solve and filter

---
+++ 2025-01-18/remove-smallest.js
@@ -0,0 +1,17 @@
+//Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list. Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+    return [];
+}
+//hint: reg fun: declare var, indexOf, Math.min, slice
+//hint: arrow: filter, indexOf, Math.min
+
+
+// my solution
+
+const removeSmallest = numbers => numbers.filter(num => indexOf(num))
+
+ 
+// right solution
+
+const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
