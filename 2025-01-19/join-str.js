a32caf462021235a064294318ce6b36c6c2147e9
Marcelo <mfuentes1205@gmail.com>
2025-01-19 12:30:35 -0800

    using temp liteerals to return parameeters

---
+++ 2025-01-19/join-str.js
@@ -0,0 +1,12 @@
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+// + , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!
+
+function joinStrings(string1, string2){
+    // code here
+}
+
+// my solution
+
+function joinStrings(string1, string2){
+    return `${string1} ${string2}`
+}
