1352b8785078f8292a864aa9fdf7cf455847c18e
Marcelo <mfuentes1205@gmail.com>
2025-01-24 13:27:52 -0800

    using template literals to solve problem:

---
+++ 2025-01-24/temp-string.js
@@ -0,0 +1,13 @@
+// Your task is to return the correct string using the Template String Feature.
+
+var templateStrings = function(noun, adjective) {
+    return //complete this
+}
+
+
+// my solution
+
+
+var templateStrings = function(noun, adjective) {
+    return `${noun} ${adjective}`
+}
