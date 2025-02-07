b68b6dd8ad91d0063689578c7fb0c95c0d8d1b40
Marcelo <mfuentes1205@gmail.com>
2025-02-06 17:24:48 -0800

    returning middle characters of words

---
+++ 2025-02-06/get-middle.js
@@ -0,0 +1,18 @@
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+function getMiddle(s){
+    //Code goes here!
+}
+//hint: can solve as arrow function, .length, .slice, Math.floor
+
+// my solution
+
+function getMiddle(s){
+    if(s % 2 !== 0){
+        return 
+    }
+}
+
+// right solution
+
+const getMiddle = s => s.length % 2 === 0 ? s.slice(s.length / 2 - 1, s.length / 2 + 1) : s[Math.floor(s.length / 2)]
