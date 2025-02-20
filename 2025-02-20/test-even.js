7e00a038639d0efe79387ac9bc140a324031dabf
Marcelo <mfuentes1205@gmail.com>
2025-02-20 13:57:06 -0800

    checking if a number is even or odd

---
+++ 2025-02-20/test-even.js
@@ -0,0 +1,17 @@
+//In this Kata we are passing a number (n) into a function.
+//Your code will determine if the number passed is even (or not).
+//The function needs to return either a true or false.
+
+function testEven(n) {
+    //Your awesome code here!
+}
+
+// my solution
+
+function testEven(n) {
+    if(n % 2 === 0){
+        return true;
+    }else{
+        return false;
+    }
+}
