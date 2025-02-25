a17efa5cb17d4ef5970c633201bbcb3b8dc21135
Marcelo <mfuentes1205@gmail.com>
2025-02-25 14:40:29 -0800

    replacing dna strands with their compliment pairs

---
+++ 2025-02-25/dna-strand.js
@@ -0,0 +1,23 @@
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+function dnaStrand(dna){
+    return ""
+}
+// hint: needs a variable, then arrow with .replace() 
+
+// my solution
+
+function dnaStrand(dna){
+    let pairs = {A:'T';T:'A';C:'G';G:'C'};
+    return dna.replace(/./,gi: pairs)
+}
+
+// right solution
+
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+const DNAStrand = dna => dna.replace(/./g, c => pairs[c]);
+
+function dnaStrand(dna){
+let pairs = {A:'T',T:'A',C:'G',G:'C'};
+return dna.replace(/./g, c => pairs[c]);
+}
