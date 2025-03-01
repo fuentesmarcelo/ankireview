244ac27b4d9ed09173733a7807ccef9bff8a1bf9
Marcelo <mfuentes1205@gmail.com>
2025-02-28 16:13:14 -0800

    checking if a car will make it to the gas station depending on mpg and fuel left

---
+++ 2025-02-28/zero-fuel.js
@@ -0,0 +1,18 @@
+//You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    //code here
+};
+// hint: can use arrow
+
+// my solution
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg + fuelLeft > distanceToPump ? yes : no;
+
+// right solution
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump/mpg <= fuelLeft
+  };
+  
+  const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
