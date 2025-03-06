6bd4072b5e049012d02c73dfe6ee72c228fc3f5c
Marcelo <mfuentes1205@gmail.com>
2025-03-06 15:16:09 -0800

    creating a function that takes three parameters and set of instructions for those parameters

---
+++ 2025-03-06/takes-3.js
@@ -0,0 +1,18 @@
+// Create a function that takes in 3 numbers. Divide the first two numbers and multiply the last. Alert the product. Call the function.
+
+// my solution
+
+function takesThree(n1, n2, n3){
+    return (n1/n2)*n3;
+}
+
+takesThree(1, 2, 3)
+call takesThree
+
+// right solution
+
+function divideAndMulti(n1,n2,n3){
+    prod = (n1/n2) * n3
+    alert(prod)
+    }
+    divideAndMulti(4,2,10)
