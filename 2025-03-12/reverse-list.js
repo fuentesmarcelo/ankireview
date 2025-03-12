0f6c8a2a3e855d0c61d507c073e7366b24d90b99
Marcelo <mfuentes1205@gmail.com>
2025-03-12 14:35:59 -0700

    reversing a list from a function

---
+++ 2025-03-12/reverse-list.js
@@ -0,0 +1,12 @@
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+function reverseList(list) {
+    //code here
+}
+//hint: reverse
+
+// my solution
+
+function reverseList(list) {
+    return list.reverse()
+}
