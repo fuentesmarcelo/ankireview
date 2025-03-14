fb72b677a6de7baaf52bc825dc3c60b2e7f04e59
Marcelo <mfuentes1205@gmail.com>
2025-03-14 14:43:16 -0700

    we are given a word in a string and we want to try to reverse it

---
+++ 2025-03-14/reverse-str.js
@@ -0,0 +1,13 @@
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+function reverse(string){
+    //your code here
+}
+//hint can be solved via arrow function
+
+// my solution
+
+function reverse(string){
+    return string.split('').reverse().join('')
+}
