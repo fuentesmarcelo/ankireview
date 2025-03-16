27489954a90e9f2d894b2de547e970bbf5c2300e
Marcelo <mfuentes1205@gmail.com>
2025-03-16 15:05:16 -0700

    using back ticks to solve problem

---
+++ 2025-03-16/find-needle.js
@@ -0,0 +1,18 @@
+//Can you find the needle in the haystack?
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+"found the needle at position " plus the index it found the needle, so:
+//Example(Input --> Output)
+//["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+// hint: .indexOf() and arrow and back ticks
+
+
+// my solution
+
+function findNeedle(){
+    return ``
+}
+
+// right solution
+
+const findNeedle = haystack => `found the needle at position ${haystack.indexOf('needle')}`;
