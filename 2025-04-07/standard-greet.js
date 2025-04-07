30cc37367c035f8464f63a6c659605a812a1b57a
Marcelo <mfuentes1205@gmail.com>
2025-04-07 13:44:25 -0700

    returning standardized message

---
+++ 2025-04-07/standard-greet.js
@@ -0,0 +1,17 @@
+// Write a function "greet" that returns "hello world!"
+
+
+// my solution
+
+function greet (word){
+    return 'hello world!'
+}
+
+
+function greet(){
+    return 'Hello world!'.toLowerCase()
+  }
+  
+  OR
+  
+  const greet = () => "hello world!";
