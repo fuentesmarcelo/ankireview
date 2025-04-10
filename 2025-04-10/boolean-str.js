99425f7b2a1a3fa399db5024ae07f45706460229
Marcelo <mfuentes1205@gmail.com>
2025-04-10 13:55:13 -0700

    checking if a boolean vlaue if the same as its str rep

---
+++ 2025-04-10/boolean-str.js
@@ -0,0 +1,28 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    //code here
+}
+//hint: if statement
+
+
+// my solution
+
+function booleanToString(b){
+    if(boolean === string){
+        return b
+    }
+}
+
+// right solutions
+function booleanToString(b){
+    if(b === true){
+      return 'true'
+    }else{
+      return 'false'
+    }
+  }
+  
+  function booleanToString(b) {
+      return b.toString();
+  }
