f64e03be4c02cc541672777b686af416efb3fd8d
Marcelo <mfuentes1205@gmail.com>
2025-04-15 15:09:13 -0700

    checking a person's bmi via if statement

---
+++ 2025-04-15/bmi.js
@@ -0,0 +1,25 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+    // code here
+}
+
+
+// my solution
+
+function bmi(weight, height) {
+    const bmi = weight / (height * height); // Calculate BMI
+    if (bmi > 30) {
+    return 'Obese';
+    } else if (bmi > 25) {
+    return 'Overweight';
+    } else if (bmi > 18.5) {
+    return 'Normal';
+    } else {
+    return 'Underweight';
+    }
+    }
