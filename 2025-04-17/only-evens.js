01f8d9c7df8beb9047a46d2c97b5703310245767
Marcelo <mfuentes1205@gmail.com>
2025-04-17 15:03:15 -0700

    returning a new array with only even numbers

---
+++ 2025-04-17/only-evens.js
@@ -0,0 +1,15 @@
+//Create and array of numbers
+//Return a new array of numbers that includes every even number from the previous Arrays
+
+function onlyEvens(arr){
+    //code here
+}
+//hint: .filter() and arrow function
+
+
+// my solution
+
+
+function onlyEvens(arr){
+    return arr.filter(num => num % 2 === 0)
+}
