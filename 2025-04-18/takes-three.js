98762129fa0f07db7f832e8b87099aef081bc676
Marcelo <mfuentes1205@gmail.com>
2025-04-18 15:00:18 -0700

    trying to find the lowest and highest values and calling the function

---
+++ 2025-04-18/takes-three.js
@@ -0,0 +1,21 @@
+//Create a function that takes in 3 numbers. Console log lowest and highest values. Call the function.
+function threeNumsLowAndHigh (n1,n2,n3) {
+    //code here
+}
+//hint: math min and max
+
+// my solution
+
+function threeNumsLowAndHigh (n1,n2,n3) {
+    let Math.min = (n1, n2, n3);
+    let Math.max = (n1, n2, n3)
+}
+
+// right solution
+
+function threeNumsLowAndHigh (n1,n2,n3) {
+    let min = Math.min(n1,n2,n3)
+    let max = Math.max(n1,n2,n3)
+    console.log(`The lowest num is ${min} and the highest num is ${max}`)
+    }
+    threeNumsLowAndHigh(100,50,25)
